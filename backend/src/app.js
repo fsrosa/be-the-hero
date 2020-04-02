691ac0d54fefe8879bdc823dfdee8481a31d2246
Fabrício Rosa <fabriciosilvarosa@gmail.com>
2020-04-01 21:54:54 -0300

    Finishing Application - Tests and Validations

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const { errors } = require('celebrate')
 const routes = require('./routes')
 
 const app = express()
@@ -7,8 +8,9 @@ const app = express()
 app.use(cors())
 app.use(express.json()) // Dizendo ao express para converser JSONs em objetos
 app.use(routes)
+app.use(errors());
 
-app.listen(3333)
+module.exports = app
 
 /**
  * Métodos HTTP:
